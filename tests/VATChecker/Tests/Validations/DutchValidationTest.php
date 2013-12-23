72619053101cf8b5f521260106f4851bb5cdf9ac
Wim Griffioen <wgriffioen@gmail.com>
2013-12-23 20:33:42 +0100

    removed the actual validation against the webservice of the EU
    Did this because it keeps failing the travis build.

--- tests/VATChecker/Tests/Validations/DutchValidationTest.php
+++ tests/VATChecker/Tests/Validations/DutchValidationTest.php
@@ -27,16 +27,5 @@ class DutchValidationTest extends \PHPUnit_Framework_TestCase
         $this->assertNotEquals(VATNumber::INVALID_COUNTRY_CODE, $vatNumber->validate());
         $this->assertNotEquals(VATNumber::INVALID_FORMAT, $vatNumber->validate());
     }
-
-    public function testValidDutchVATNumber()
-    {
-        // This is the VAT number of KPN NV (http://www.kpn.nl)
-        $vatNumber = new VATNumber('NL009292056B01');
-
-        $this->assertNotEquals(VATNumber::EMPTY_VAT_NUMBER, $vatNumber->validate());
-        $this->assertNotEquals(VATNumber::INVALID_COUNTRY_CODE, $vatNumber->validate());
-        $this->assertNotEquals(VATNumber::INVALID_FORMAT, $vatNumber->validate());
-        $this->assertEquals(VATNumber::VALID_VAT_NUMBER, $vatNumber->validate());
-    }
 }
  
