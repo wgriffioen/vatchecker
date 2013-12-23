268134713851692ef7fcc300c9d2807306cdf74a
Wim Griffioen <wgriffioen@gmail.com>
2013-12-23 20:35:35 +0100

    made the bootstrap file check for the existence of the autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,10 @@
 <?php
 
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    echo "You must install the dependencies using:\n";
+    echo "    composer install --dev\n";
+    exit(1);
+}
+
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->add('VATChecker\Tests', __DIR__);
